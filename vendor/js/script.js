58dc697057bed8e70e4d3e8c09eb841b3f2d8240
glinsky off <omsk.den@bk.ru>
2022-06-10 18:03:20 +0600

    first deploy

---
+++ vendor/js/script.js
@@ -0,0 +1,81 @@
+
+$(function () {
+  $('.main-slider').slick({
+    arrows: false,
+    dots: true,
+    sliderToShow: 1,
+    sliderToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 2500,
+    dotsClass: 'main-slider-dots',
+  })
+
+})
+
+$(function () {
+  $('.one-slider').slick({
+    arrows: false,
+    dots: true,
+    sliderToShow: 1,
+    sliderToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dotsClass: 'one-slider-dots',
+    draggable: true,
+    fade: true,
+    touchThreshold: 100,
+    centerMode: true
+  })
+
+})
+
+$(function () {
+  $('.two-slider').slick({
+    arrows: false,
+    dots: true,
+    sliderToShow: 1,
+    sliderToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dotsClass: 'two-slider-dots',
+    draggable: true,
+    fade: true,
+    touchThreshold: 100,
+    centerMode: true
+  })
+
+})
+
+$(function () {
+  $('.three-slider').slick({
+    arrows: false,
+    dots: true,
+    sliderToShow: 1,
+    sliderToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dotsClass: 'three-slider-dots',
+    draggable: true,
+    fade: true,
+    touchThreshold: 100,
+    centerMode: true
+  })
+
+})
+
+$(function () {
+  $('.four-slider').slick({
+    arrows: false,
+    dots: true,
+    sliderToShow: 1,
+    sliderToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    dotsClass: 'four-slider-dots',
+    draggable: true,
+    fade: true,
+    touchThreshold: 100,
+    centerMode: true
+  })
+
+})
